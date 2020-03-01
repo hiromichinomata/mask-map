24792d0c568b680387c5e5db0f45b366b636083c
Hiromichi NOMATA <git@hiromichinomata.com>
2020-03-01 10:16:26 +0900

    [remove] symfony

--- scripts/_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return array(
-    'tgos' => array(
-        'url' => 'http://addr.tgos.nat.gov.tw/addrws/v30/QueryAddr.asmx/QueryAddr',
-        'APPID' => '',
-        'APIKey' => '',
-    ),
-);
